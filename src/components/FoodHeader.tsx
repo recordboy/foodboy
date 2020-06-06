235278aaa5e075b49b7843a548c1c5aba4c9d61f
recordboy <ksj_design@naver.com>
2020-06-07 01:21:36 +0900

    foodman init

---
+++ src/components/FoodHeader.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class FoodHeader extends Component {
+    render() {
+        return(
+            <div>foodman test</div>
+        )
+    }
+
+}
+
+export default FoodHeader;
