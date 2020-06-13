90fb75dc0d8cdcd7e867f005e10298e090809790
recordboy <ksj_design@naver.com>
2020-06-13 16:47:12 +0900

    이벤트 테스트

--- src/components/FoodForm.tsx
+++ src/components/FoodForm.tsx
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 
 class FoodForm extends Component {
+  
+  handleAdd = () => {
+    console.log(1)
+  }
+
+
 	render() {
 		return (
 			<div className="header">
 				<input type="text" id="input-txt" className="inputTxt" />
 				<div className="btnArea">
-					<button type="button" id="btn-add" className="btnAdd">add</button>
+					<button type="button" onClick={this.handleAdd}>add</button>
 					<button type="button" id="btn-all-del" className="btnAllDel">all delete</button>
 					<button type="button" id="btn-mix" className="btnMix">mix</button>
 					<button type="button" id="btn-select" className="btnSelect">select</button>
