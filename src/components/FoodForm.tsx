2e2f81900b5cf9a9bdd37de7122587da4a6ffa14
recordboy <ksj_design@naver.com>
2020-06-20 13:08:30 +0900

    작업

--- src/components/FoodForm.tsx
+++ src/components/FoodForm.tsx
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
 
 class FoodForm extends Component {
-  
-  handleAdd = () => {
-    console.log(1)
+
+  state = {
+    item: ''
   }
 
+  handleAdd = (e: any) => {
+    console.log(this.state.item)
+  }
 
-	render() {
-		return (
-			<div className="header">
-				<input type="text" id="input-txt" className="inputTxt" />
-				<div className="btnArea">
-					<button type="button" onClick={this.handleAdd}>add</button>
-					<button type="button" id="btn-all-del" className="btnAllDel">all delete</button>
-					<button type="button" id="btn-mix" className="btnMix">mix</button>
-					<button type="button" id="btn-select" className="btnSelect">select</button>
-				</div>
-			</div>
-		)
-	}
+  handleData = (e: any) => {
+    this.setState({
+      item: e.target.value
+    });
+
+  }
+
+  render() {
+    return (
+      <div className="header">
+        <input type="text" id="input-txt" className="inputTxt" onChange={this.handleData} />
+        <div className="btnArea">
+          <button type="button" onClick={this.handleAdd}>add</button>
+          <button type="button" id="btn-all-del" className="btnAllDel">all delete</button>
+          <button type="button" id="btn-mix" className="btnMix">mix</button>
+          <button type="button" id="btn-select" className="btnSelect">select</button>
+        </div>
+      </div>
+    )
+  }
 
 }
 
