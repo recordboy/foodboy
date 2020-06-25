69d420d4ab95203adb434f75e546ce1629c62ee9
recordboy <ksj_design@naver.com>
2020-06-25 19:24:45 +0900

    훅 추가

--- src/components/FoodList.tsx
+++ src/components/FoodList.tsx
@@ -1,13 +1,18 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class FoodList extends Component {
+const FoodList = () => {
   
-  render() {
-    return (
-      <ul id="list-food" className="listFood"></ul>
-    )
-  }
+  const [count, setCount] = useState(0);
 
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
 }
+
 
 export default FoodList;
