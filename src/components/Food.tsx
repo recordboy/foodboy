fb959072a9549594dabce7a6d172be9fc2933bfa
recordboy <ksj_design@naver.com>
2020-08-01 01:16:13 +0900

    클래스 변경 시작

--- src/components/Food.tsx
+++ src/components/Food.tsx
@@ -1,25 +1,53 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import FoodForm from './FoodForm';
 import FoodList from './FoodList';
 
-let id = 0;
-const info: object[] = [];
-const Food = () => {
-  const [list, setList] = useState([{}]);
-  const createList = (inputData: string) => {
-    if (inputData) {
-      info.push({ id: id++, inputData });
-      setList(info);
-      console.log(info);
-    }
+class Food extends Component {
+  id = 0;
+  state = {
+    // information: ['1', '2'],
+    information: 0,
   };
-  const deleteList = () => {};
-  return (
-    <>
-      <FoodForm createList={createList} deleteList={deleteList} />
-      <FoodList data={info} />
-    </>
-  );
-};
+
+  createList = (inputData: string) => {
+    const { information } = this.state;
+
+    this.setState({
+      // information: information.push('inputData'),
+      information: information + 1,
+    });
+
+    console.log(information);
+  };
+  deleteList = () => { };
+  render() {
+    return (
+      <>
+        {/* <FoodForm createList={this.createList} /> */}
+        {/* <FoodList data={info} /> */}
+      </>
+    );
+  }
+}
+
+// let id = 0;
+// const info: object[] = [];
+// const Food = () => {
+//   const [list, setList] = useState([{}]);
+//   const createList = (inputData: string) => {
+//     if (inputData) {
+//       info.push({ id: id++, inputData });
+//       setList(info);
+//       console.log(info);
+//     }
+//   };
+//   const deleteList = () => {};
+//   return (
+//     <>
+//       <FoodForm createList={createList} deleteList={deleteList} />
+//       <FoodList data={info} />
+//     </>
+//   );
+// };
 
 export default Food;
