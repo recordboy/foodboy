497de0af72446d7a919331518bec4709e346e354
Record Man <ksj_design@naver.com>
2020-07-31 21:28:56 +0900

    폼 영역 레이아웃 변경

--- src/components/FoodForm.tsx
+++ src/components/FoodForm.tsx
@@ -1,34 +1,48 @@
 import React, { useState } from 'react';
 const FoodForm = (props: {
-  inputData: string,
-  setInputData: (data: string) => void;
   createList: (data: string) => void;
+  deleteList: () => void;
 }) => {
-  const { inputData, setInputData,
-    createList } = props;
+  const [inputData, setInputData] = useState('');
+  const { createList, deleteList } = props;
   return (
-    <div className="header">
-      <input type="text" id="input-txt" className="inputTxt" value={inputData} onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-        setInputData(e.target.value);
-      }} />
+    <div className="search">
+      <input
+        type="text"
+        id="input-txt"
+        className="inputTxt"
+        value={inputData}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          setInputData(e.target.value);
+        }}
+      />
       <div className="btnArea">
-        <button type="button" onClick={() => {
-          createList(inputData);
-          setInputData("");
-        }}>add</button>
-        <button type="button" id="btn-all-del" className="btnAllDel">all delete</button>
-        <button type="button" id="btn-mix" className="btnMix">mix</button>
-        <button type="button" id="btn-select" className="btnSelect">select</button>
+        <button
+          type="button"
+          onClick={() => {
+            createList(inputData);
+            setInputData('');
+          }}
+        >
+          add
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            deleteList();
+          }}
+        >
+          all delete
+        </button>
+        <button type="button" id="btn-mix" className="btnMix">
+          mix
+        </button>
+        <button type="button" id="btn-select" className="btnSelect">
+          select
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default FoodForm;
-
-
-
-
-
-
-
