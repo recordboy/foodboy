120b79b31b31f641d4272ccdef4a583ee41c7609
recordboy <ksj_design@naver.com>
2020-08-01 14:26:13 +0900

    삭제 버튼 수정

--- src/components/FoodItem.tsx
+++ src/components/FoodItem.tsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
-const FoodItem = (props: { key: number; name: string }) => {
-  return <div>{props.name}<button type="button">delete</button></div>;
+const FoodItem = (props: { key: number; name: string, deleteItem: (id: number) => void; }) => {
+  const { key } = props;
+  return <div>{props.name}<button type="button" onClick={() => {
+    console.log(key);
+
+  }}>delete</button></div>;
 };
 export default FoodItem;
