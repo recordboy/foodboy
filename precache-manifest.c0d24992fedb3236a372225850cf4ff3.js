fc4894d673a7d156cb4b3e43a5b1b46a642d3a95
recordboy <ksj_design@naver.com>
2020-08-07 16:33:47 +0900

    Updates

---
+++ precache-manifest.c0d24992fedb3236a372225850cf4ff3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4f5da6e9c068250c784793ff2f8b29f",
+    "url": "/foodboy/index.html"
+  },
+  {
+    "revision": "f9ca331fa87a025f0cc6",
+    "url": "/foodboy/static/css/main.f1d2f01f.chunk.css"
+  },
+  {
+    "revision": "c09bf0b5f30ca2231fa8",
+    "url": "/foodboy/static/js/2.a5852e13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/foodboy/static/js/2.a5852e13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9ca331fa87a025f0cc6",
+    "url": "/foodboy/static/js/main.e312c462.chunk.js"
+  },
+  {
+    "revision": "0db1ba28d716dabb19d4",
+    "url": "/foodboy/static/js/runtime-main.77319407.js"
+  }
+]);
